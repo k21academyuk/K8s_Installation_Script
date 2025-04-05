32a47ff769ebc5a8f9be759461fc6fad7d09b477
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2025-04-05 12:58:30 +0530

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 apt-get install -y curl openssh-server vim 
-sed -e 's/^.*PermitRootLogin prohibit-password/PermitRootLogin yes/g' -i  /etc/ssh/sshd_config
-systemctl restart sshd 
+sudo sed -i '/^#PermitRootLogin prohibit-password/a PermitRootLogin yes' /etc/ssh/sshd_config
+sudo sed -i 's/^#ClientAliveInterval 0/ClientAliveInterval 20/' /etc/ssh/sshd_config
+sudo sed -i 's/^#ClientAliveCountMax 3/ClientAliveCountMax 30/' /etc/ssh/sshd_config
+systemctl restart ssh
 systemctl disable --now ufw
 
 tee /etc/modules-load.d/containerd.conf <<EOF
@@ -41,9 +43,9 @@ systemctl enable containerd
 
 apt-get update && apt-get install -y apt-transport-https ca-certificates curl
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update && apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
